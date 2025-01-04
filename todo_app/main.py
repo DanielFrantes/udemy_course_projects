7a3476348db803e2c58ebe1a049c6f472afcce44
Daniel Frantes <danfrantes@seznam.cz>
2025-01-04 13:17:01 +0100

    Ask for input endlessly with while loop

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,7 @@
 user_prompt = "Type todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("next...")
+    
